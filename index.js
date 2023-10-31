fa48f81a9c48cc6aa17d76bb2ee96a1183aa5887
Mohammed Aslam <mohammedaslam4106@gmail.com>
2023-10-31 16:13:58 +0530

    small error

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const routesApi=require('./routes/api/index')
 const dotenv=require('dotenv')
 dotenv.config()
 const MONGO_DB_CONNECTION=process.env.MONGO_DB_CONNECTION
-console.log(MONGO_DB_CONNECTION)
 
 mongoose.connect(`${MONGO_DB_CONNECTION}/generate_link`).then(res=>console.log('connected to mongoose')).catch(error=>console.log(error))
 
 const api=express()
 
-api.use(express.json())
+api.use(express.json({limit: '50mb'}))
 api.use(cors())
 
 api.use('/api',routesApi)
